9cbabc44212d336ebf1ba339a9e0a4e91bb52634
kikodrex <50791270+kikodrex@users.noreply.github.com>
2025-06-24 00:46:48 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'b4dcc4d7e05faa999e28002a02c3607a';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
